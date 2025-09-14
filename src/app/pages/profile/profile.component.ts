80e4b0b038b22acc878d1a2d6631ef836c95b942
Karimov Islom <71114810+Islom1999@users.noreply.github.com>
2025-09-14 18:54:50 +0500

    Updated profile.component.ts

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { CommonModule, OnInit } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
