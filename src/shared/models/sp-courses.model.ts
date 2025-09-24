5b7a7a04818c346c52c606850df804347f3e0923
Islom1999 <islomkarimovalisherovich@gmail.com>
2025-09-24 11:39:58 +0500

    change some module

--- src/shared/models/sp-courses.model.ts
+++ src/shared/models/sp-courses.model.ts
@@ -1,5 +1,17 @@
 import { IBaseModel } from '../../core/base/base.model';
 
+export enum PremiumType {
+  free = 'free',
+  premium = 'premuim',
+}
+
+export enum LessonType {
+  text = 'text',
+  video_youtube = 'video_youtube',
+  video_server = 'video_server',
+  gibrid = 'gibrid',
+}
+
 export interface ISpCourses extends IBaseModel {
   name: string;
   name_uz?: string;
@@ -21,4 +33,25 @@ export interface ISpCourses extends IBaseModel {
   code: string;
   sp_category_id: string;
   sp_level_id: string;
+  sp_courses_modules: ISpCoursesModule[];
+}
+
+export interface ISpCoursesModule extends IBaseModel {
+  name_uz: string;
+  name_ru: string;
+  name_kr: string;
+  duration: string;
+  sp_courses_id: string;
+  sp_courses_module_parts: ISpCoursesModulePart[];
+}
+
+export interface ISpCoursesModulePart extends IBaseModel {
+  file_video_id: string;
+  name_uz: string;
+  name_ru: string;
+  name_kr: string;
+  duration: string;
+  type: LessonType;
+  content?: string;
+  sp_courses_module_id: string;
 }
