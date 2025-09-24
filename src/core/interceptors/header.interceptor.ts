b87fde0dfb06caf4b72dc8a4e2203c3176ad75b7
Karimov Islom <71114810+Islom1999@users.noreply.github.com>
2025-09-24 14:54:29 +0500

    Updated header.interceptor.ts

--- src/core/interceptors/header.interceptor.ts
+++ src/core/interceptors/header.interceptor.ts
@@ -1,10 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const headerInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = localStorage.getItem('accessToken');
   const clonedReq = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-    },
+    setHeaders: token ? { Authorization: `Bearer ${token}` } : {},
   });
   return next(clonedReq);
 };
